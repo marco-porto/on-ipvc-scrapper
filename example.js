99a34c65288d612fc9fce292967874c64444b575
Marco Porto <50577988+4SMarcoPorto@users.noreply.github.com>
2021-01-09 19:50:53 +0000

    update

--- example.js
+++ example.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
+
 const { login } = require('./lib')
-
 ;(async () => {
-	let user = await login('amatossousa', 'password')
+	let user = await login(process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD)
 	console.log(user)
-	let schedule = await user.getSchedule('202021', 'S1', 'EI-1-C', '311')
-
+	let schedule = await user.getSchedule('202021', 'S1', 'EI-1-A', '319')
+	
 	console.log(schedule)
 })()
