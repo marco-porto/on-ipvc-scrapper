1333c4e2e4154a99bbc36ed9ef46e43552235edf
André Sousa <andrematosdesousa@gmail.com>
2021-02-24 23:26:07 +0000

    Added schedule by date. Renamed and modified some response fields

--- example.js
+++ example.js
@@ -4,7 +4,11 @@ const { login } = require('./lib')
 ;(async () => {
 	let user = await login(process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD)
 	console.log(user)
-	let schedule = await user.getSchedule('202021', 'S1', 'EI-1-A', '319')
-	
+	let schedule = await user.getSchedule('202021', 'S2', 'EI-1-B', '326')
 	console.log(schedule)
+
+
+	let scheduleByDate = await user.getScheduleByDate('202021', 'S2', 'EI-1-B', '2021', '03', '24')
+	console.log(scheduleByDate)
+
 })()
